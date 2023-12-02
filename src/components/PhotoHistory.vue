1ff51e7f502de94d6a6c8743d70cfbad64bce5f5
jllado <jllado@gmail.com>
2023-12-02 19:34:22 +0100

    PhotoHistory: added fat info

--- src/components/PhotoHistory.vue
+++ src/components/PhotoHistory.vue
@@ -21,6 +21,9 @@
             <div class="center">
               {{ photo.weight.dateFormat }}
             </div>
+            <div class="center">
+              {{ photo.weight.fat }} kg ({{ photo.weight.fat_percentage }}%) <span v-bind:class="{'bad': photo.weight.lost_fat > 0, 'good': photo.weight.lost_fat <= 0}">{{ photo.weight.lost_fat > 0 ? '+' : '' }}{{ photo.weight.lost_fat }}kg</span>
+            </div>
             <div>
               <img :src="photo.photo_left" style="width: 100%"  v-if="photo_type == 'left'" />
               <img :src="photo.photo_front" style="width: 100%" v-if="photo_type == 'front'" />
