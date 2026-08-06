ae7395799e3ff4fa861debb913fb63f5a60ba982
jllado <jllado@gmail.com>
2026-08-06 23:09:26 +0200

    Revert routine push notifications

--- backend/src/main/java/com/jllado/weightcontrol/repository/PushSubscriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jllado.weightcontrol.repository;
-
-import com.jllado.weightcontrol.domain.PushSubscription;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PushSubscriptionRepository extends JpaRepository<PushSubscription, Long> {
-    Optional<PushSubscription> findByEndpointHash(String endpointHash);
-}
--- backend/src/main/java/com/jllado/weightcontrol/service/PushGateway.java
+++
@@ -1,7 +0,0 @@
-package com.jllado.weightcontrol.service;
-
-import com.jllado.weightcontrol.domain.PushSubscription;
-
-public interface PushGateway {
-    int send(PushSubscription subscription, String payload, int ttlSeconds);
-}
