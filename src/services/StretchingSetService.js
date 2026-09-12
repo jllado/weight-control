5af312ddea5013e140654ca2939ac46236e273c0
jllado <jllado@gmail.com>
2026-09-12 16:15:58 +0200

    Fix stretching set update request payload

--- src/services/StretchingSetService.js
+++ src/services/StretchingSetService.js
@@ -2,6 +2,6 @@ import {del, get, post, put} from './api';
 
 export default {
     get_all() { return get('/stretching-sets'); },
-    save(set) { return set.id ? put(`/stretching-sets/${set.id}`, set) : post('/stretching-sets', set); },
+    save({id, name, entries}) { return id ? put(`/stretching-sets/${id}`, {name, entries}) : post('/stretching-sets', {name, entries}); },
     delete(id) { return del(`/stretching-sets/${id}`); }
 };
